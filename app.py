96af08947bf8fbb0565ca1c77d3ee7c6a5d691c4
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2017-08-28 12:46:15 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+  return jsonify(
+    message = "Hello from my Clusterized app"
+  )
+
+if __name__ == '__main__':
+  app.run(
+    host = '0.0.0.0',
+    port = int("4000")
+  )
