6c1fdb83f02fd2721f39d54f9dd4c238e540aec9
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2017-09-11 15:09:19 -0300

    Update app to return node and pod info

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 from ubuntu:latest
 MAINTAINER Gabriel Vinha "gabriel.vinha@outlook.com"
 
+ARG NODE_NAME
+
 #Instala pip + python dependencias
 RUN apt-get update -y
 RUN apt-get install -y python-pip python-dev build-essential
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import socket
 from flask import Flask, jsonify
 app = Flask(__name__)
@@ -5,11 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
   return jsonify(
-    message = "Hello from %s" %socket.gethostbyname(socket.gethostname())
+    message = "Hello from pod %s running at node %s" %(socket.gethostname(), os.environ["NODE_NAME"])
   )
 
 if __name__ == '__main__':
   app.run(
-    host = '0.0.0.0',
+    host= "0.0.0.0",
     port = int("8080")
   )
